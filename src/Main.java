ccbfa6f22aa919e4521732c51141b7d3874dabc8
kovrik <kovrik0@gmail.com>
2014-04-20 15:52:57 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import kovrik.cardinal.HyperLogLog;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(r);
-        System.out.println(Integer.toBinaryString(r));
-        HyperLogLog hll = new HyperLogLog();
-        int elements = 10000000;
+        HyperLogLog hll1 = new HyperLogLog();
+        int elements = 20000000;
         for (int i = 0; i < elements; i++) {
-            hll.add(new Object());
+            hll1.add(new Object());
         }
-        double cardinality = hll.cardinality();
+
+        HyperLogLog hll2 = new HyperLogLog();
+        for (int i = 0; i < elements; i++) {
+            hll2.add(new Object());
+        }
+        hll1.merge(hll2);
+
+        elements = 2*elements; // merged
+        double cardinality = hll1.cardinality();
         System.out.println("Elements: " + elements);
         System.out.println("Cardinality: " + cardinality);
 
         double epsilon = (1.0 - (cardinality / elements)) * 100;
         System.out.println("Epsilon actual: " + epsilon + "%");
 
-        System.out.println("Absolute epsilon expected: " + hll.standardError() * 100 + "%");
-        System.out.println("Number of registers: " + hll.numberOfRegisters());
-        System.out.println(hll);
+        System.out.println("Absolute epsilon expected: " + hll1.standardError() * 100 + "%");
+        System.out.println("Number of registers: " + hll1.size());
+        System.out.println(hll1);
 
     }
 }
